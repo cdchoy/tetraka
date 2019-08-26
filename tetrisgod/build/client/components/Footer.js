8426f47a7b2bc8fdf582987443fac90e8bf6631b
Chris Choy <contact@chrischoy.net>
2019-08-26 15:55:13 -0500

    got the jsx to compile to js

---
+++ tetrisgod/build/client/components/Footer.js
@@ -0,0 +1,12 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var react_1 = __importDefault(require("react"));
+function Footer() {
+    return (react_1.default.createElement("footer", null,
+        react_1.default.createElement("h3", null, "This is our footer.")));
+}
+exports.default = Footer;
+//# sourceMappingURL=Footer.js.map
