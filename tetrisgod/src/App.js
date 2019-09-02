3e0d486d7f973e03d8d7ba6e3fdb46f2dee622ea
Chris Choy <contact@chrischoy.net>
2019-09-02 09:52:31 -0500

    built a working navbar

--- tetrisgod/src/App.js
+++ tetrisgod/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './css/App.css';
 import Header from "./Header"
 
@@ -7,21 +6,6 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          Howdy
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
--- tetrisgod/src/Header.js
+++ tetrisgod/src/Header.js
@@ -1,16 +1,28 @@
 import React from "react"
+import logo from "./logo.svg";
 
 class Header extends React.Component {
     constructor() {
-        super()
-        this.state = {}
+        super();
+        this.state = {};
     }
 
     render() {
         return (
             <div>
-                <h1> this is a header </h1>
-                <p> we could put a navbar here!</p>
+                <nav>
+                    <ul className="Navbar">
+                        <li className="NavItem"><a href="/" className="NavItemText">Play</a></li>
+                        <li className="NavItem"><a href="/" className="NavItemText">Leaderboards</a></li>
+                        <li className="NavItem"><a href="/" className="NavItemText">About</a></li>
+                    </ul>
+                </nav>
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        Under Development. Pardon our mess.
+                    </p>
+                </header>
             </div>
         )
     }
