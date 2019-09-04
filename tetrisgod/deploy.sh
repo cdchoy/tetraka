957e24a835482d86798a3650107e7bfe0e217c58
Chris Choy <contact@chrischoy.net>
2019-09-04 12:55:59 -0500

    added some potential cmds

--- tetrisgod/deploy.sh
+++ tetrisgod/deploy.sh
@@ -2,5 +2,6 @@
 
 npm run build
 cd ..
+#git push heroku master
 #git subtree push --prefix tetrisgod heroku hosting:master
 git push heroku `git subtree split --prefix tetrisgod master`:master --force
