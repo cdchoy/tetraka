e22d12ab951c390860345dedf2f8be8d80bd2d1a
Chris Choy <contact@chrischoy.net>
2019-09-05 00:22:46 -0500

    added some checks for proper deploy script execution

--- tetrisgod/deploy.sh
+++ tetrisgod/deploy.sh
@@ -1,7 +1,25 @@
 #!/usr/bin/env bash
+# Script to push tetrisgod/tetrisgod/ directory onto heroku remote
 
-# push master branch's tetrisgod/ appdir to heroku remote
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+
+if [[ "${PWD##*/tetrisgod/}" != "tetrisgod" ]]; then
+	echo -e >&2 "${RED}ERROR: Deploy script must be executed from its own directory.${NC}"
+	echo >&2 "Aborting..."
+	exit 1;
+fi
+
+if [[ "$BRANCH" != "master" ]]; then
+  echo -e >&2 "${RED}ERROR: We should only deploy from the master branch version to production!${NC}";
+  echo >&2 'Aborting...';
+  exit 1;
+fi
 
 cd ..
 git subtree push --prefix tetrisgod heroku master
-#git push heroku `git subtree split --prefix tetrisgod master`:master --force
+
+# If you're getting a weird issue with branch tips, you can run...
+# git push heroku `git subtree split --prefix tetrisgod master`:master --force
