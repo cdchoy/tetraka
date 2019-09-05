71f23d9067f3c3eaa420bdd1797075de4544171c
Chris Choy <contact@chrischoy.net>
2019-09-04 23:45:06 -0500

    updated App to as it should be for our app

--- tetrisgod/react-ui/src/App.jsx
+++ tetrisgod/react-ui/src/App.jsx
@@ -1,66 +1,22 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import logo from './media/logo.svg';
+import React from 'react';
 import './css/App.css';
+import {Route} from "react-router";
+import LandingPage from "./components/LandingPage";
+import HomePage from "./components/HomePage";
+import PlayPage from "./components/PlayPage";
+import TodoPage from "./components/TodoPage";
+import AboutPage from "./components/AboutPage";
 
 function App() {
-    const [message, setMessage] = useState(null);
-    const [isFetching, setIsFetching] = useState(false);
-    const [url, setUrl] = useState('/api');
-
-    const fetchData = useCallback(() => {
-        fetch(url)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(`status ${response.status}`);
-                }
-                return response.json();
-            })
-            .then(json => {
-                setMessage(json.message);
-                setIsFetching(false);
-            }).catch(e => {
-            setMessage(`API call failed: ${e}`);
-            setIsFetching(false);
-        })
-    }, [url]);
-
-    useEffect(() => {
-        setIsFetching(true);
-        fetchData();
-    }, [fetchData]);
-
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                { process.env.NODE_ENV === 'production' ?
-                    <p>
-                        This is a production build from create-react-app.
-                    </p>
-                    : <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                }
-                <p>{'« '}<strong>
-                    {isFetching
-                        ? 'Fetching message from API'
-                        : message}
-                </strong>{' »'}</p>
-                <p><a
-                    className="App-link"
-                    href="https://github.com/mars/heroku-cra-node"
-                >
-                    React + Node deployment on Heroku
-                </a></p>
-                <p><a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn Reactsasd
-                </a></p>
-            </header>
+        <div>
+            <div className="App-intro">
+                <Route exact path="/" component={LandingPage}/>
+                <Route path="/home" component={HomePage}/>
+                <Route path="/play" component={PlayPage}/>
+                <Route path="/leaderboards" component={TodoPage}/>
+                <Route path="/about" component={AboutPage}/>
+            </div>
         </div>
     );
 
