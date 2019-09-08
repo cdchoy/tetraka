63f851d6b92411a12cbd92cc2cbf00beb7400f0d
Chris Choy <contact@chrischoy.net>
2019-09-07 20:57:05 -0500

    typified index.ts

--- tetrisgod/server/index.ts
+++ tetrisgod/server/index.ts
@@ -1,109 +1,130 @@
-import {Input} from "./models/Input";
+import * as HTTP from "http";
+import * as Path from "path";
+import * as Cluster from "cluster";
+import * as Express from "express";
 import * as SocketIO from "socket.io";
 
-const cluster = require('cluster');
-const isDev = process.env.NODE_ENV !== 'production';
-const port = process.env.PORT || 5000;
+import {Input} from "./models/Input";
 
-/** Multi-process to utilize all CPU cores. */
-if (!isDev && cluster.isMaster) {
+/**
+ * Multi-process to utilize all CPU cores.
+ * */
+function startClusterMaster() {
   const numCPUs = require('os').cpus().length;
 
   console.error(`Node cluster master ${process.pid} is running`);
 
   // Fork workers.
   for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
+    Cluster.fork();
   }
 
-  cluster.on('exit', (worker, code, signal) => {
+  Cluster.on('exit', (worker, code, signal) => {
     console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
   });
-
 }
 
-/** Normal Single-instance server hosting */
-else {
-  /** SERVER SETUP */
-  const express = require('express');
-  const path = require('path');
-  const app = express();
-  const server = require('http').Server(app);
-  const io = require('socket.io')(server);
+/**
+ * Normal Single-instance server hosting
+ * */
+class MyServer {
+  public static readonly PORT: number = 5000;
 
-  // Begin Listening for socket & app communications
-  server.listen(port);
+  readonly app: Express.Application;
+  readonly port: string | number;
+  readonly server: HTTP.Server;
+  private io: SocketIO.Server;
 
-  // Priority serve any static files.
-  app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+  private SOCKET_LIST: Array<any> = new Array<any>();
 
-  // Answer API requests.
-  app.get('/api', function (req, res) {
-    res.set('Content-Type', 'application/json');
-    res.send('{"message":"Hello from the custom server!"}');
-  });
+  constructor() {
+    this.app = Express();
+    this.port = process.env.PORT || MyServer.PORT;
+    this.setupApp();
+    this.server = HTTP.createServer(this.app);
+    this.io = SocketIO(this.server);
+    this.listen();
+  }
 
-  // Answer PINGs
-  app.get('/ping', function (req, res) {
-    return res.send('pong');
-  });
+  private setupApp(): void {
+    this.app.use(Express.static(Path.resolve(__dirname, '../react-ui/build')));
 
-  // All remaining requests return the React app, so it can handle routing.
-  app.get('*', function(request, response) {
-    response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
-  });
-  /** END SERVER SETUP */
+    // Answer API requests.
+    this.app.get('/api', (req, res) => {
+      res.set('Content-Type', 'application/json');
+      res.send('{"message":"Hello from the custom server!"}');
+    });
 
+    // Answer PINGs
+    this.app.get('/ping', (req, res) => {
+      return res.send('pong');
+    });
 
-  /** SOCKET EVENT HANDLER */
-  let SOCKET_LIST : Array<SocketIO.Socket> = new Array<SocketIO.Socket>();
+    // All remaining requests return the React app, so it can handle routing.
+    this.app.get('*', (request, response) => {
+      response.sendFile(Path.resolve(__dirname, '../react-ui/build', 'index.html'));
+    });
+  }
 
-  io.on('connection', (socket) => {
-    onConnect(socket);
-    socket.on('disconnect', () => { onDisconnect(socket); });
-    // socket.on('keyPress', () => { onKeyPress(socket); })
+  private listen(): void {
+    // listen on our defined port
+    this.server.listen(this.port, () => {
+      console.log('Running server on port %s', this.port);
+    });
 
-  });
+    // socket events
+    this.io.on('connection', (client) => {
+      this.onConnect(client);
+      client.on('disconnect', () => { this.onDisconnect(client); });
+      client.on('keyPress', () => { this.onKeyPress(client); })
+    });
 
-  const onConnect = (socket: SocketIO.Socket) => {
-    socket.id = Math.random();
-    socket.keyInput = new Input();
-    SOCKET_LIST[socket.id] = socket;
+    // runs every frame
+    setInterval(() => {
+      for (let socket of this.SOCKET_LIST) {
+        socket.emit('test');
+      }
+    },1000/25);  // 25fps
+  }
+
+  private onConnect = (client: any) => {
+    client.id = Math.random();
+    client.keyInput = new Input();
+    this.SOCKET_LIST[client.id] = client;
   };
 
-  const onDisconnect = (socket: SocketIO.Socket) => {
-    delete SOCKET_LIST[socket.id];
+  private onDisconnect = (client: any) => {
+    delete this.SOCKET_LIST[client.id];
   };
 
-  const onKeyPress = (socket: SocketIO.Socket) => {
+  private onKeyPress = (client: any) => {
     console.log("KEY PRESSED!");
-    if (socket.data.inputId === 'moveleft')
-      socket.keyInput.pressingMoveLeft = socket.data.state;
-    else if (socket.data.inputId === 'rotateright')
-      socket.keyInput.pressingRotateRight = socket.data.state;
-    else if (socket.data.inputId === 'moveright')
-      socket.keyInput.pressingMoveRight = socket.data.state;
-    else if (socket.data.inputId === 'softdrop')
-      socket.keyInput.pressingSoftDrop = socket.data.state;
-    else if (socket.data.inputId === 'harddrop')
-      socket.keyInput.pressingHardDrop = socket.data.state;
-    else if (socket.data.inputId === 'hold')
-      socket.keyInput.pressingHold = socket.data.state;
+    if (client.data.inputId === 'moveleft')
+      client.keyInput.pressingMoveLeft = client.data.state;
+    else if (client.data.inputId === 'rotateright')
+      client.keyInput.pressingRotateRight = client.data.state;
+    else if (client.data.inputId === 'moveright')
+      client.keyInput.pressingMoveRight = client.data.state;
+    else if (client.data.inputId === 'softdrop')
+      client.keyInput.pressingSoftDrop = client.data.state;
+    else if (client.data.inputId === 'harddrop')
+      client.keyInput.pressingHardDrop = client.data.state;
+    else if (client.data.inputId === 'hold')
+      client.keyInput.pressingHold = client.data.state;
   };
-
-
-  setInterval(() => {
-    for (let socket of SOCKET_LIST) {
-      socket.emit('test');
-      // let game = GAME_LIST[socket.id];
-      // game.update(socket);
-    }
-  },1000/25);  // 25fps
-
-  /** END SOCKET EVENT HANDLER */
 }
 
 
+function main() {
+  const isDev = process.env.NODE_ENV !== 'production';
+  if (!isDev && Cluster.isMaster) {
+    startClusterMaster();
+  } else {
+    new MyServer();
+  }
+}
+
+main();
 
 
 
